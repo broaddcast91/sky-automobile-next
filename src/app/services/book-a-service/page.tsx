0c55eb1b759c1706a02eb7aadc9013db40ef5d71
salman170 <salmanansari02120@gmail.com>
2024-09-03 11:39:39 +0530

    done with adding state

--- src/app/services/book-a-service/page.tsx
+++ src/app/services/book-a-service/page.tsx
@@ -58,7 +58,7 @@ const BookAService: React.FC = () => {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log("Form Data:", formData);
+    console.log("Form Data:", { ...formData, state: 'Odisa' });
     toast.success("Thank You for contacting us. We will get back to you soon!");
 
     // Uncomment if you want to reset the form after submission
